ff1b1c4a5f0080e9519aa238c384c1494b8bddb3
atechnologyalfred <adjekotaa@gmail.com>
2026-03-04 15:03:39 -0800

    add to data.js

--- data.js
+++ data.js
@@ -2,17 +2,26 @@
         {
             id: 1,
             name: 'Alfred',
-            gender: 'Male'
+            gender: 'Male',
+            tech: 'Software developer'
         },
         {
             id: 2,
-            name: 'Amos',
-            gender: 'Male'
+            name: 'Donata',
+            gender: 'Female',
+            tech: 'Frontend developer'
         },
         {
             id: 3,
             name: 'Charisma',
-            gender: 'Female'
+            gender: 'Female',
+            tech: 'Forest trader'
+        },
+        {
+            id:4,
+            name: 'Grace strongPack',
+            gender: 'Female',
+            tech: 'Data analyst'
         }
     ]
     export default data;
