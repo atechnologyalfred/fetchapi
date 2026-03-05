d997d9357174781c320b5ffd77d67cb993b8c0ca
atechnologyalfred <adjekotaa@gmail.com>
2026-03-05 02:55:30 -0800

    last commit done

--- data.js
+++ data.js
@@ -2,34 +2,61 @@
         
         {
             id: 1,
-            name: 'Alfred',
+            name: 'King Sherif',
             gender: 'Male',
             tech: 'Software developer', 
             age: 30,
-            movement: [4000, 3000, 7000, 4900]
+            movement: ['Reactjs', 'Nextjs', 'Angular', 'python', 'mongoDb', 'Golang']
         },
+
         {
             id: 2,
+            name: 'Moderator',
+            gender: 'Male',
+            tech: 'Software developer', 
+            age: 30,
+            movement: ['Reactjs', 'Nextjs', 'python', 'mongoDb', 'etc']
+        },
+                {
+            id: 3,
+            name: 'moderator',
+            gender: 'Male',
+            tech: 'Software developer', 
+            age: 30,
+            movement: ['Reactjs', 'Nextjs', 'python', 'Nodejs', 'mongoDb']
+        },
+
+        {
+            id: 4,
             name: 'Donata',
             gender: 'Female',
             tech: 'Frontend developer',
-            age: 40, 
-            movement: [500, 600, 3400, -500]
+            age: 20, 
+            technologies: ['html', 'custom css', 'tailwindcss', 'reactjs', 'git and github']
         },
         {
-            id: 3,
+            id: 5,
             name: 'Charisma',
             gender: 'Female',
             tech: 'Forest trader',
-            age: 50,
-            movement: [40, 900, 600, 500]
+            age: 23,
+            movement: ['meta forest', 'trading view']
         },
         {
-            id:4,
-            name: 'Grace strongPack',
+            id:6,
+            name: 'Grace',
             gender: 'Female',
             tech: 'Data analyst',
+            age: 27,
             movement: [400, 700, 3450, 45.56]
+        },
+                {
+            id:7,
+            name: 'Evangel',
+            gender: 'Female',
+            tech: 'Graphic designer, UI/UX designer & Frontend web developer',
+            age: 24,
+            technologies: ['html', 'custom css', 'tailwindcss', 'reactjs', 'git and github', 'canva', 'photoshop', 'corelDraw', 'inDesign', 'Adobe Illustrator' ]
         }
     ]
 
